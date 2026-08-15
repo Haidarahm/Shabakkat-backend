5cafd903dacdeb67e0393bbc0333c0d2bb5f043b
Haidar <haidarahmad421@gmail.com>
2026-08-15 13:05:03 +0300

    Add new partner entries in PartnerSeeder for enhanced diversity
    - Included additional partners: Alfa, ATC, Cummins, Daikin, Enviro, GO, Rogers, Vodafone, and Zain, expanding the range of partners represented in the database seeder.

--- database/seeders/PartnerSeeder.php
+++ database/seeders/PartnerSeeder.php
@@ -12,10 +12,16 @@ class PartnerSeeder extends Seeder
         $partners = [
             ['name' => 'Airtel', 'logo_src' => '/images/logo/airtel.webp'],
             ['name' => 'Alcatel-Lucent', 'logo_src' => '/images/logo/alcatel-lucent.webp'],
+            ['name' => 'Alfa', 'logo_src' => '/images/logo/alfa.jpg'],
+            ['name' => 'ATC', 'logo_src' => '/images/logo/atc.jpg'],
             ['name' => 'Azady', 'logo_src' => '/images/logo/Azady.webp'],
             ['name' => 'Celtel', 'logo_src' => '/images/logo/celtel-seeklogo.webp'],
+            ['name' => 'Cummins', 'logo_src' => '/images/logo/cummins.jpg'],
+            ['name' => 'Daikin', 'logo_src' => '/images/logo/daikin.jpg'],
             ['name' => 'EEMC', 'logo_src' => '/images/logo/eemc.webp'],
+            ['name' => 'Enviro', 'logo_src' => '/images/logo/enviro.jpg'],
             ['name' => 'Ericsson', 'logo_src' => '/images/logo/Ericsson.webp'],
+            ['name' => 'GO', 'logo_src' => '/images/logo/go-etihad-atheeb.jpg'],
             ['name' => 'GTE', 'logo_src' => '/images/logo/GTE_logo.webp'],
             ['name' => 'Huawei', 'logo_src' => '/images/logo/huawei-logo.webp'],
             ['name' => 'IHS', 'logo_src' => '/images/logo/ihs-logo.webp'],
@@ -24,8 +30,11 @@ class PartnerSeeder extends Seeder
             ['name' => 'Nokia', 'logo_src' => '/images/logo/nokia-seeklogo.webp'],
             ['name' => 'Ooredoo', 'logo_src' => '/images/logo/Ooredoo.webp'],
             ['name' => 'Oracle', 'logo_src' => '/images/logo/oracle.webp'],
+            ['name' => 'Rogers', 'logo_src' => '/images/logo/rogers.jpg'],
             ['name' => 'STC', 'logo_src' => '/images/logo/STC.webp'],
             ['name' => 'TNSS', 'logo_src' => '/images/logo/TNSS.webp'],
+            ['name' => 'Vodafone', 'logo_src' => '/images/logo/vodafone.jpg'],
+            ['name' => 'Zain', 'logo_src' => '/images/logo/zain.jpg'],
             ['name' => 'ZTE', 'logo_src' => '/images/logo/ZTE.webp'],
         ];
 
